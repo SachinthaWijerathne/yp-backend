ba044dbead410c8f6cbdcf06da002d49e3310cea
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-11-16 23:24:08 +0530

    2

--- api/send-email.js
+++ api/send-email.js
@@ -11,6 +11,12 @@ const transporter = nodemailer.createTransport({
 
 // Export the handler as a serverless function
 module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end();
+    }
     if (req.method !== 'POST') {
         return res.status(405).send({ success: false, message: 'Method not allowed' });
     }
