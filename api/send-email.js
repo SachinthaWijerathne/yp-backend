2a54212bdc7310ad49989abf60c0e1fe7a664c1c
Sachintha <tharindu.sachintha.xyz1@gmail.com>
2024-12-15 13:03:01 +0530

    update

--- api/send-email.js
+++ api/send-email.js
@@ -16,6 +16,7 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  
   if (req.method === "OPTIONS") {
     return res.status(204).end();
   }
